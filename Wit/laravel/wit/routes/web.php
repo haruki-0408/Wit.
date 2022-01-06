597cf1e24789d5930aa384f238abfb153f1ef18b
haruki-0408 <haru0408g1@gmail.com>
2022-01-06 22:11:31 +0900

    css動いた！

--- Wit/laravel/wit/routes/web.php
+++ Wit/laravel/wit/routes/web.php
@@ -24,3 +24,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/bootstrap', function () {
+    return view('bootstrap');
+});
