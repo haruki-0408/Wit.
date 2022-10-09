bc5903b4f7d83ea775bd54caa1d9f28bdcda77e0
haruki-0408 <haru0408g1@gmail.com>
2022-10-09 18:49:17 +0900

    index.blade.php 途中

--- Wit/laravel/wit/resources/views/wit/index.blade.php
+++ Wit/laravel/wit/resources/views/wit/index.blade.php
@@ -17,57 +17,74 @@
 </head>
 
 <body>
-
     <div class="container-fluid content">
         <header>
             <nav class="navbar navbar-expand-md navbar-light bg-transparent">
                 <a class="navbar-brand" href="#">Wit.</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03"
-                    aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
             </nav>
         </header>
 
         <main>
-            <div class="top-message mx-auto">
+            <div class="Top mx-auto">
                 <div class="row justify-content-center">
                     <div class="col-8">
-                        <img width=200px height=200px src="{{ asset('/images/wit/wit.png') }} "">
-                        Welcome to Wit.
+                        <img width=100px height=100px src="{{ asset('/images/wit/wit.png') }} "">
+                        <span class="text-center">Welcome to Wit.</span>
                     </div>
                 </div>
             </div>
 
-            <div class="top-button  mb-5">
-                <div class="row justify-content-center">
-                    <div class="col-2">
-                    </div>
-                    <div class="col-sm-12 align-self-center col-md-3"> <a href="{{ route('login') }}"
-                            class="btn btn-primary px-5 m-2  ">ログイン</a> </div>
-                    <div class="col-sm-12 align-self-center col-md-3"> <a href="{{ route('register') }}"
-                            class="btn btn-success px-5 m-2  ">新規登録</a> </div>
-                </div>
+            <div class="row text-center m-2">
+                <p class="text-dark fw-bold" style="font-style:initial">質問　議論　コミュニティ</p>
+            </div>
+            
+            <div class="Auth-Button row justify-content-center mb-2">
+                <div class="col-sm-12 text-center col-md-3"> <a href="{{ route('login') }}"
+                        class="btn btn-primary px-4 m-2  ">ログイン</a> </div>
+                <div class="col-sm-12 text-center col-md-3"> <a href="{{ route('register') }}"
+                        class="btn btn-success px-4 m-2  ">新規登録</a> </div>
             </div>
 
-            <div class="message-wrapper mx-auto"></div>
-            <div class="row">
-                <div id="PC" class="col-lg-5 col-sm-12 text-center"><video width="600" src="movie/pc.mp4" autoplay muted loop></video></div>
-                <div id="Tablet" class="col-lg-3 col-sm-12 text-center mx-auto"><video width="400" src="movie/tb.mp4" autoplay muted loop></video></div>
-                <div id="Smartphone" class="col-lg-2 col-sm-12 text-center mx-auto"><video width="200" src="movie/sm.mp4" autoplay muted loop></video></div>
+
+            <div class="Icon row d-flex justify-content-center">
+                <div id="PC" class="col-lg-3 col-md-12 col-12 text-center">
+                    <div class="rounded-circle border-1 bg-white m-2">
+                        <svg width="20" height="20" fill="currentColor" class="bi bi-display"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M0 4s0-2 2-2h12s2 0 2 2v6s0 2-2 2h-4c0 .667.083 1.167.25 1.5H11a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h.75c.167-.333.25-.833.25-1.5H2s-2 0-2-2V4zm1.398-.855a.758.758 0 0 0-.254.302A1.46 1.46 0 0 0 1 4.01V10c0 .325.078.502.145.602.07.105.17.188.302.254a1.464 1.464 0 0 0 .538.143L2.01 11H14c.325 0 .502-.078.602-.145a.758.758 0 0 0 .254-.302 1.464 1.464 0 0 0 .143-.538L15 9.99V4c0-.325-.078-.502-.145-.602a.757.757 0 0 0-.302-.254A1.46 1.46 0 0 0 13.99 3H2c-.325 0-.502.078-.602.145z" />
+                        </svg> PC
+                    </div>
+                    <video width="100%" src="movie/pc.mp4" autoplay muted loop></video>
+                </div>
+                <div id="Tablet" class="col-lg-3 col-md-6 col-12 text-center">
+                    <div class="rounded-circle border-1 bg-white m-2">
+                        <svg width="20" height="20" fill="currentColor" class="bi bi-tablet" viewBox="0 0 16 16">
+                            <path
+                                d="M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z" />
+                            <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                        </svg> Tablet
+                    </div>
+                    <video width="100%" src="movie/tb.mp4" autoplay muted loop></video>
+                </div>
+                <div id="Smartphone" class="col-lg-2 col-md-6 col-12 text-center">
+                    <div class="rounded-circle border-1 bg-white m-2">
+                        <svg width="20" height="20" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
+                            <path
+                                d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+                            <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                        </svg> Phone
+                    </div>
+                    <video width="100%" src="movie/sm.mp4" autoplay muted loop></video>
+                </div>
             </div>
     </div>
     </main>
 
     <!-- FOOTER -->
-    <footer class="container m-5 bg-transparent">
-        <p class="float-end"><a href="#">Back to top</a></p>
-        <p>&copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a>
-        </p>
+    <footer class="row m-1 bg-transparent fixed-bottom">
+        <p class="d-inline-block">Copyright © 2022 Wit. All Rights Reserved. <a href="#">Terms of Service</a></p>
     </footer>
-
-    </div>
-
 
 
 
