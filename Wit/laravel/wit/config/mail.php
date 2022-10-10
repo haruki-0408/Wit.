891f854dca4b57110e0a45fb46cf1bab8e3ff035
haruki-0408 <haru0408g1@gmail.com>
2022-10-10 21:38:31 +0900

    mysql data .gitignoreに追加

--- Wit/laravel/wit/config/mail.php
+++ Wit/laravel/wit/config/mail.php
@@ -36,10 +36,9 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            //'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'host' => env('MAIL_HOST'),
+            'port' => env('MAIL_PORT'),
+            'encryption' => env('MAIL_ENCRYPTION'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
