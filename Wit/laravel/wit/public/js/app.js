aa9f20a10c8c1ceb5269490928113d494b1eb225
haruki-0408 <haru0408g1@gmail.com>
2022-10-20 21:33:35 +0900

    CodeDeploy用設定ファイルappspec.ymlを作成

--- Wit/laravel/wit/public/js/app.js
+++ Wit/laravel/wit/public/js/app.js
@@ -5386,7 +5386,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "5163a9411cc7edaaaaf6",
+  key: "a7cafce9dcc5986f2063",
   cluster: "ap3",
   forceTLS: true
 });
