42df2e33f81a7bc3195e3f4ca37595e6679712df
haruki-0408 <haru0408g1@gmail.com>
2022-10-25 21:11:46 +0900

    deploy.sh npm install 追加

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ chmod -R 777 public/ && \
 cd public && \
 rm -r userImages && \
 cd /var/www/laravel/wit/ && \
+npm install &&\
 npm run dev && \
 php artisan storage:link && \
 php artisan config:clear 
