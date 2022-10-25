86f2a4396c3b1967ed3593c7ef933592c8cb9ed0
haruki-0408 <haru0408g1@gmail.com>
2022-10-25 21:15:55 +0900

    deploy.sh npm run dev削除

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,5 @@ chmod -R 777 public/ && \
 cd public && \
 rm -r userImages && \
 cd /var/www/laravel/wit/ && \
-npm install &&\
-npm run dev && \
 php artisan storage:link && \
 php artisan config:clear 
